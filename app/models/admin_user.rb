e46d172e0bc2f7913d23656e5e2c655ea79eb915
Chris Cole <nystigmus1@mac.com>
2013-04-08 21:30:03 -0700

    Add migration for Join Table (admin_users_pages).  Setup many-to-many relationship between page and admin_user.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,3 +1,4 @@
 class AdminUser < ActiveRecord::Base
+	attr_accessible :first_name, :last_name, :email, :hashed_password, :username, :salt
 	has_and_belongs_to_many :pages
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -2,5 +2,5 @@ class Page < ActiveRecord::Base
   attr_accessible :name, :permalink, :position, :subject_id, :visible
   belongs_to :subject
   has_many :sections
-  has_and_belongs_to_many :admin_users
+  has_and_belongs_to_many :editors, :class_name => "AdminUser"
 end
