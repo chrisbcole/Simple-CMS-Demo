6340d4699896bfd135927a957eb0486865dc8e2d
Chris Cole <nystigmus1@mac.com>
2013-04-09 10:13:04 -0700

    Add CRUD for Page and Section.  Include drop downs for Subject and Page in Page and Section Create and Edit pages respectively.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,46 @@
 class PagesController < ApplicationController
+	def index
+		list
+		render('list')
+	end
+	def list
+		@pages = Page.order("pages.position ASC")
+	end
+	def show
+		@page = Page.find(params[:id])
+	end
+	def new
+		@subjects = Subject.all
+		@page = Page.new(:name => 'default')
+	end
+	def create
+		@page = Page.new(params[:page])
+		if @page.save
+			flash[:notice]= "Page created."
+			redirect_to(:action => 'list')
+		else
+			render('new')
+		end
+	end
+	def edit
+		@subjects = Subject.all
+		@page = Page.find(params[:id])
+	end
+	def update
+		@page = Page.find(params[:id])
+		if @page.update_attributes(params[:page])
+			flash[:notice]= "Page updated."
+			redirect_to(:action => 'show', :id => @page.id)
+		else
+			render('edit')
+		end
+	end
+	def delete
+		@page = Page.find(params[:id])
+	end
+	def destroy
+		Page.find(params[:id]).destroy
+		flash[:notice]= "Page destroyed."
+		redirect_to(:action => 'list')
+	end
 end
