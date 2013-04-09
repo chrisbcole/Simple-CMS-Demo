84a9b0728d5206512b9151347d969be72ef52026
Chris Cole <nystigmus1@mac.com>
2013-04-08 21:03:32 -0700

    Add one-to-one relationship between Subject and Page (1)

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,4 @@
 class Page < ActiveRecord::Base
   attr_accessible :name, :permalink, :position, :subject_id, :visible
+  belongs_to :subject
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,10 @@
 class Subject < ActiveRecord::Base
   attr_accessible :name, :position, :visible
+
+  has_one :page
+
+  # examples of named scopes
+  scope :visible, where(:visible => true)
+  scope :invisible, where(:visible => false)
+  scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"])}
 end
