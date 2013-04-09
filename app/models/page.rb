9da0cecdf0acbb2c4286285565237f22e3b4f5fb
Chris Cole <nystigmus1@mac.com>
2013-04-08 21:20:03 -0700

    Add has_and_belongs_to_many relation between admin_users and pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,5 @@ class Page < ActiveRecord::Base
   attr_accessible :name, :permalink, :position, :subject_id, :visible
   belongs_to :subject
   has_many :sections
+  has_and_belongs_to_many :admin_users
 end
